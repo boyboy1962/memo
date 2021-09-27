db56e245139b5ea470a959bdf98b23c4d0cd1038
301-05\User <boyboy1962@gmail.com>
2021-09-27 22:02:56 +0900

    ★☆ 수업 내용 ☆★ depreected -> 삭제할 예정 문자에서 취소선이 그려져 있으면 이후 버전에서 삭제를 할 수 있어 사용하진 않는 것을 추천한다.
    .properties -> .yml
    application.yml 로 사용하는 것이 좋다.
    그래서 application.properties 삭제 되고 application.yml을 새로 만들었다.
    
    DB 설정후 결과에서 확인해야 하는 것
    json 파일을 잘 띄울 수 있는지
    loger가 잘 나오는지
    
    tiles 뷰를 나눌 수 있는 프레임 워크(나중에 찾아봐라)
    
    ★1. DB용 ★
    1. MemoApplication.java 기존 라인 삭제
    여기에서는 임시로 DB를 읽지 않도록 하는 것을 삭제를 진행했다.
    만약 해당 라인이 없다면 그냥 무시하자
    
    2. DataBaseConfig.java 신규
    DB를 사용할 수 있게 준비용
    여기서는 라인 14번째와 22번째를 잘보자
    14번째라인에는 패키지 경로를 잘 설정을 해줘야 한다. 주로 "com." 이후의 문자가 다르다
    22번은 변경이 필요한 것은 아니고 resource 쪽에 "mappers" 페키지를 넣는 것을 잊지 않으려는 용도임.
    
    3. application.yml 신규
    DB를 사용할 수 있게 준비용
    여기는 라인 5번의 시작으로 7번 부터 11번의 내용이 DB를 위한 것임
    여기는 사실 써져있는 주석을 잘보자...
    
    4. logback-spring.xml 신규
    DB의 로그를 볼 수 있게 해주는 용
    여기 12번 라인에 name="com." 이후에 있는 내용을 잘 적어 두자
    
    4. TestController.java 메소드 추가
    25 ~30번 라인을 확인하자.
    여기는 DB를 테스트 하기 위한 코드가 있다.
    List<Map<String, Object>>는 model에 테이블 정보를 만들지 않고 바로 쓰려고 할때 할 수 있다.(테이블이랑 메핑되지않는 객체를 만들지 않고 바로 쓸 수 있게 한거죠)
    
    5. TestBO.java // TestDAO.java // testMapper.xml 신규
    여기에는 가장 기본이 되는 select를 사용하기 위한 BO, DAO, Mapper를 준비함
    다만 주의할 점은 Mapper에서 사용하는 *을 절대 사용하지 말것 지금은 테스트용도라 그냥 씀
    
    ★2. JSP용 ★
    1. build.gradle 추가
    여기는 해당라인을 추가하고 Refrash Gradle Project 해야한다.
    그러지 않으면 404에러가 뜨거 님 너무 당황해서 시간 낭비 존나함
    
    2. application.yml 신규
    JSP를 사용할 수 있게 준비용
    여기는 라인 5번의 시작으로 15번 부터 18번의 내용이 JSP를 위한 것임
    여기는 사실 써져있는 주석을 잘보자...
    
    3. TestController.java 메소드 추가
    32 ~36번 라인을 확인하자.
    여기는 JSP를 테스트 하기 위한 코드가 있다.
    
    4. test.jsp 신규
    간단한 테스트를 위한 내용임

--- src/main/java/com/memo/MemoApplication.java
+++ src/main/java/com/memo/MemoApplication.java
@@ -1,11 +1,9 @@
 package com.memo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})//임시로 DB를 사용하지 않는다.
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})//임시로 DB를 사용하지 않는다.
 @SpringBootApplication
 public class MemoApplication {
 
--- src/main/java/com/memo/test/TestController.java
+++ src/main/java/com/memo/test/TestController.java
@@ -1,16 +1,38 @@
 package com.memo.test;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.memo.test.bo.TestBO;
+
 @Controller
 public class TestController {
+	
+	@Autowired
+	private TestBO testBO;
 
 	@ResponseBody
 	@RequestMapping("/test1")
 	public String test () {
 		return "hello masterJ!!";
+	}
+	
+	//DB 연동 테스트
+	@RequestMapping("/test2")
+	@ResponseBody
+	public List<Map<String, Object>> test2(){//임시로 map으로 만들었다. 
+		return testBO.getUserList();
+	}
+	
+	// jsp 연동 테스트
+	@RequestMapping("/test3")
+	public String test3() {
+		return "template/layout";
 	}
 	
 	@RequestMapping("/user/sign_in_view")
