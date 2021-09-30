8d2072b383532194a8eb987a57c5ae86ba3a0254
301-05\User <boyboy1962@gmail.com>
2021-09-30 22:05:40 +0900

    글목록 올리기 작업
    미래의 나야... 지금의 내가 눈치 보면서 커밋을 하고 있으니 제발 그냥 딱보고 잘 알기를 바란다.
    고맙다. 어렵지는 않을꺼야 ㅠㅠ

---
+++ src/main/java/com/memo/post/PostController.java
@@ -0,0 +1,43 @@
+package com.memo.post;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.memo.post.bo.PostBO;
+import com.memo.post.model.Post;
+
+@Controller
+@RequestMapping("/post")
+public class PostController {
+
+	private Logger logger = LoggerFactory.getLogger(PostController.class);
+	
+	@Autowired
+	private PostBO postBO;
+	
+	@RequestMapping("post_list_view")
+	public String postListView(Model model, HttpServletRequest request) {
+		// 글 목록들을 가져온다.
+		HttpSession session = request.getSession();
+		Integer userId = (Integer)session.getAttribute("userId");
+		if (userId == null) {
+			logger.info("[post_list_view] userId is null. " + userId);
+			return "redirect:/user/sign_in_view";
+		}
+		
+		List<Post> postList = postBO.getPostList(userId);
+		//모델에 담는다.
+		
+		model.addAttribute("viewName", "post/post_list");
+		return "template/layout";
+	}
+}
