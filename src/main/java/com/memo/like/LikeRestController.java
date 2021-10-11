40f371af4173227b438dc6d6bda2a255b09ecd9a
301-05\User <boyboy1962@gmail.com>
2021-10-11 17:06:57 +0900

    올라가지 못한 내용임

--- src/main/java/com/memo/like/LikeRestController.java
+++ src/main/java/com/memo/like/LikeRestController.java
@@ -1,5 +1,24 @@
 package com.memo.like;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LikeRestController {
+	
+	@PostMapping("like/{postId}")
+	public Map<String, Object> postLike(
+				@PathVariable int postId
+				, HttpServletRequest request
+			) {
+		
+		//글번호, 유저번호
+		
+	}
 
 }
